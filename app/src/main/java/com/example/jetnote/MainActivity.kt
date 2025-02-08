3caa1e55f1afad78926ffb9f053f4642a30ea1f5
Matheus Souza <matheus621cardoso@gmail.com>
2025-02-08 14:56:31 -0300

    implementing the viewmodel

--- app/src/main/java/com/example/jetnote/MainActivity.kt
+++ app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetnote
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,7 +13,9 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.jetnote.model.Note
+import com.example.jetnote.screens.NoteViewModel
 import com.example.jetnote.screens.NotesScreen
 import com.example.jetnote.ui.theme.JetNoteTheme
 
@@ -25,22 +28,26 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colors.background
                 ) {
-                    val notes = remember {
-                        mutableStateListOf<Note>()
-                    }
-                    NotesScreen(notes = notes,
-                        onAddNote = {
-                            notes.add(it)
-                        },
-                        onRemoveNote = {
-                            notes.remove(it)
-                        })
+                    val noteViewModel: NoteViewModel by viewModels()
+                    NotesApp(noteViewModel = noteViewModel)
                 }
             }
         }
     }
 }
 
+
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
+    val notesList = noteViewModel.getAllNotes()
+    NotesScreen(notes = notesList,
+        onAddNote = {
+            noteViewModel.addNote(it)
+        },
+        onRemoveNote = {
+            noteViewModel.removeNote(it)
+        })
+}
 
 @Preview(showBackground = true)
 @Composable
